811d5b697d19cb530acb51b62df0dba4b0bcf41e
takekai822 <kaikisoccer78@gmail.com>
2022-07-05 02:09:35 +0000

    usersテーブルにintroductionカラム追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_07_05_015801) do
+ActiveRecord::Schema.define(version: 2022_07_05_020820) do
 
   create_table "books", force: :cascade do |t|
     t.string "title"
@@ -29,6 +29,7 @@ ActiveRecord::Schema.define(version: 2022_07_05_015801) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "name"
+    t.string "introduction"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
