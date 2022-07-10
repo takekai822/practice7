f1e8c4a122c198d80ee500a5b9641440f55b1e7f
takekai822 <kaikisoccer78@gmail.com>
2022-07-10 07:11:38 +0000

    レイアウト調整

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :authenticate_user!, except: [:top, :about]
 
   def after_sign_in_path_for(resource)
     user_path(current_user)
@@ -12,6 +13,6 @@ class ApplicationController < ActionController::Base
 
   protected
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up,keys:[:email])
+    devise_parameter_sanitizer.permit(:sign_up, keys:[:email])
   end
 end
